093be1c0c3bd6e36f5f5ac937917a7f6237abd5a
vaibhava17 <iamvaibhav.agarwal@gmail.com>
2023-04-18 15:14:59 +0530

    gh-pages deploy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import ChatBot from "./ChatBot";
 const App = () => {
   return (
     <div className="App">
-      <Header containerClass="justify-content-center" dark={true}>
+      <Header containerClass="justify-content-center" dark={true} >
         ChatBot Without AI
       </Header>
       <ChatBot />
